53d4ed5e277bad3a71c84650b71f0761f5b1751f
c1ph3rbyt3 <c1ph3rbyt3@proton.me>
2025-12-30 15:57:11 -0600

    Flags HTTPS

--- backend/src/interfaces/http/middlewares/enforceHttps.js
+++ backend/src/interfaces/http/middlewares/enforceHttps.js
@@ -1,8 +1,22 @@
 import { AppError } from '../../../shared/errors.js';
+import logger from '../../../infra/logging/logger.js';
 
-const enforceHttps = (req, _res, next) => {
+const enforceHttps = (req, res, next) => {
   const proto = (req.headers['x-forwarded-proto'] || req.protocol || '').toString().toLowerCase();
   if (proto === 'https') return next();
+
+  logger.warn(
+    {
+      requestId: res.locals?.requestId,
+      proto,
+      host: req.headers.host,
+      path: req.originalUrl,
+      ip: req.ip,
+      forwardedFor: req.headers['x-forwarded-for'],
+      tag: 'HTTP_ENFORCE_HTTPS'
+    },
+    'Rejected non-HTTPS request'
+  );
   return next(new AppError('HTTPS required', 426));
 };
 
